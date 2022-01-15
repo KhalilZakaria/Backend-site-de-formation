6a62ef6526ebcdf113e7eb2b591a75749af50756
Zakaria KHALIL <zakaria.khalil1999@gmail.com>
2022-01-15 15:10:32 +0100

    Modification backend

--- src/main/java/com/example/Formations/Repository/FormateurRepository.java
+++ src/main/java/com/example/Formations/Repository/FormateurRepository.java
@@ -4,6 +4,7 @@ package com.example.Formations.Repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.example.Formations.entities.Categorie;
 import com.example.Formations.entities.Client;
@@ -17,5 +18,7 @@ public interface FormateurRepository extends JpaRepository<Formateur,Long> {
 	Formateur findByNom(String nom);
 	public Formateur findById(long id);
 	public List<Formateur> findAll();
+	@Query(value = "SELECT max(id) FROM formateur", nativeQuery = true)
+	Long getLastCreated();
 
 }
